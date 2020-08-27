745e9ba0a83e5318a3d66a2e48aab448310aeea2
Maria Eugenia <meug.ferreira@gmail.com>
2020-08-27 18:52:29 -0300

    Atualizando arquivos

---
+++ menuhamburguer.js
@@ -0,0 +1,11 @@
+
+//PARA ATIVAR O MENU HAMBURGUER
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
